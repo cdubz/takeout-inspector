2ce30f9017c9111cc38c5c927891f7678cb9ad35
Chris Wells <chris@chrxs.net>
2016-10-28 18:47:03 -0400

    Added a function to import messages with some key message information.

--- parsers/sqlite.py
+++ parsers/sqlite.py
@@ -7,13 +7,47 @@ class Parser:
         self.email = mailbox.mbox(mbox_file)
         self.conn = sqlite3.connect(db_file)
 
+    def import_messages(self):
+        c = self.conn.cursor()
+        c.execute('''
+                    CREATE TABLE IF NOT EXISTS messages(
+                      `message_key` INT,
+                      `from` TEXT,
+                      `to` TEXT,
+                      `subject` TEXT,
+                      `date` TEXT,
+                      `gmail_thread_id` INT,
+                      `gmail_labels` TEXT
+                     );
+                 ''')
+        self.conn.commit()
+
+        count = 0
+        for key, message in self.email.items():
+            mail_from = message.get('From', message.get('from', message.get('FROM', ''))).decode('utf-8')
+            mail_to = message.get('To', message.get('to', message.get('TO', ''))).decode('utf-8')
+            mail_subject = message.get('Subject', message.get('subject', message.get('SUBJECT', ''))).decode('utf-8')
+            mail_date = message.get('Date', message.get('date', message.get('DATE', ''))).decode('utf-8')
+            mail_gmail_id = message.get('X-GM-THRID', '')
+            mail_gmail_labels = message.get('X-Gmail-Labels', '').decode('utf-8')
+
+            c.execute('''INSERT INTO `messages` VALUES(?, ?, ?, ?, ?, ?, ?);''',
+                      (key, mail_from, mail_to, mail_subject, mail_date, mail_gmail_id, mail_gmail_labels))
+
+            count += 1
+            if count > 100000000:
+                self.conn.commit()
+                count = 0
+
+        self.conn.commit()
+
     def import_message_headers(self):
         c = self.conn.cursor()
         c.execute('''
             CREATE TABLE IF NOT EXISTS headers(
-              message_key INT,
-              header TEXT,
-              value TEXT
+              `message_key` INT,
+              `header` TEXT,
+              `value` TEXT
              );
          ''')
         self.conn.commit()
@@ -21,7 +55,7 @@ class Parser:
         count = 0
         for key, message in self.email.items():
             for header, value in message.items():
-                c.execute('''INSERT INTO headers VALUES(?, ?, ?);''', (key, header, value.decode('utf-8')))
+                c.execute('''INSERT INTO `headers` VALUES(?, ?, ?);''', (key, header, value.decode('utf-8')))
             count += 1
             if count > 100000000:
                 self.conn.commit()
@@ -33,8 +67,8 @@ class Parser:
         c = self.conn.cursor()
         c.execute('''
                     CREATE TABLE IF NOT EXISTS headers_occurrences(
-                      header VARCHAR(255) PRIMARY KEY,
-                      occurrences INT
+                      `header` TEXT PRIMARY KEY,
+                      `occurrences` INT
                      );
                  ''')
 
@@ -47,6 +81,6 @@ class Parser:
                     headers[header] += 1
 
         for header, occurrences in headers.iteritems():
-            c.execute('''INSERT INTO headers_occurrences VALUES(?, ?);''', (header, occurrences))
+            c.execute('''INSERT INTO `headers_occurrences` VALUES(?, ?);''', (header, occurrences))
 
         self.conn.commit()
