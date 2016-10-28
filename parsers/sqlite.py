3fcd8dc673a405ed4d18064fd9bb7d1b0d7c3ffa
Chris Wells <chris@chrxs.net>
2016-10-28 18:52:58 -0400

    Remove header occurrences function. This type of functionality will come later in other modules.

--- parsers/sqlite.py
+++ parsers/sqlite.py
@@ -62,25 +62,3 @@ class Parser:
                 count = 0
 
         self.conn.commit()
-
-    def create_headers_occurrences_table(self):
-        c = self.conn.cursor()
-        c.execute('''
-                    CREATE TABLE IF NOT EXISTS headers_occurrences(
-                      `header` TEXT PRIMARY KEY,
-                      `occurrences` INT
-                     );
-                 ''')
-
-        headers = {}
-        for message in self.email:
-            for header in message.keys():
-                if header not in headers:
-                    headers[header] = 1
-                else:
-                    headers[header] += 1
-
-        for header, occurrences in headers.iteritems():
-            c.execute('''INSERT INTO `headers_occurrences` VALUES(?, ?);''', (header, occurrences))
-
-        self.conn.commit()
