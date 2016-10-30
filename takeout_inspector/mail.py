9ef8683b7d8f1525ff0e7ab0585778d2f6ee32a7
Chris Wells <chris@chrxs.net>
2016-10-29 23:21:38 -0400

    Refactor main import method in to smaller methods - one for each table being populated.

--- takeout_inspector/mail.py
+++ takeout_inspector/mail.py
@@ -1,6 +1,6 @@
 """takeout_inspector/mail.py
 
-Defines classes and methods used to parse a Gmail mbox file from Google Takeout and import data in to an sql database.
+Defines classes and methods used to parse a Google Takeout mbox file and import data in to an sqlite database.
 
 Copyright (c) 2016 Christopher Charbonneau Wells
 
@@ -31,14 +31,17 @@ from datetime import datetime
 
 
 class Import:
-    """Parses and imports Google Takeout mbox file data in to sqlite."""
+    """Parses and imports Google Takeout mbox file data in to sqlite.
+    """
     def __init__(self, mbox_file, db_file):
         self.email = mailbox.mbox(mbox_file)
         self.conn = sqlite3.connect(db_file)
-        self.create_tables()
+        self._create_tables()
+        self.query_count = 0
 
-    def create_tables(self):
-        """Creates the required tables for message data storage. Indexes will be added after data import."""
+    def _create_tables(self):
+        """Creates the required tables for message data storage. Indexes will be added after data import.
+        """
         c = self.conn.cursor()
         c.execute('''
             CREATE TABLE IF NOT EXISTS messages(
@@ -70,51 +73,62 @@ class Import:
         self.conn.commit()
 
     def import_messages(self):
-        """Imports message details in to the `messages` table and all message headers in to the `headers` table."""
+        """Imports message details in to the `messages` table and all message headers in to the `headers` table.
+        """
         c = self.conn.cursor()
 
-        query_count = 0
         for key, message in self.email.items():
-            """Adds all headers to `headers`."""
-            for header, value in message.items():
-                c.execute('''INSERT INTO `headers` VALUES(?, ?, ?);''', (key, header, value.decode('utf-8')))
-                query_count += 1
-
-            """Creates a basic index of important message data in `messages`."""
-            mail_from = message.get('From', '').decode('utf-8')
-            mail_to = message.get('To', '').decode('utf-8')
-            mail_subject = message.get('Subject', '').decode('utf-8')
-            mail_date_utc = self._get_message_date(message)
-            mail_gmail_id = message.get('X-GM-THRID', '')
-            mail_gmail_labels = message.get('X-Gmail-Labels', '').decode('utf-8')
-
-            c.execute('''INSERT INTO `messages` VALUES(?, ?, ?, ?, ?, ?, ?);''',
-                      (key, mail_from, mail_to, mail_subject, mail_date_utc, mail_gmail_id, mail_gmail_labels))
-            query_count += 1
-
-            """Parses contents of the To and CC headers for unique email addresses to be added to the
-            one-row-per-address `recipients` table."""
-            mail_all_to = message.get_all('To', [])
-            mail_all_cc = message.get_all('CC', [])
-            unique_recipients = list(set(email.utils.getaddresses(mail_all_to + mail_all_cc)))
-            for name, address in unique_recipients:
-                c.execute('''INSERT INTO `recipients` VALUES(?, ?, ?);''',
-                          (key, name.decode('utf-8'), address.decode('utf-8')))
-                query_count += 1
-
-            if query_count > 100000000:
-                self.conn.commit()
-                query_count = 0
+            self._insert_messages(c, key, message)
+            self._insert_headers(c, key, message)
+            self._insert_recipients(c, key, message)
 
         c.execute('''CREATE INDEX `id_date` ON `messages` (`date` DESC)''')
 
         self.conn.commit()
 
+    def _insert_recipients(self, c, key, message):
+        """Parses contents of the To and CC headers for unique email addresses to be added to the one-row-per-address
+        `recipients` table.
+        """
+        mail_all_to = message.get_all('To', [])
+        mail_all_cc = message.get_all('CC', [])
+        unique_recipients = list(set(email.utils.getaddresses(mail_all_to + mail_all_cc)))
+        for name, address in unique_recipients:
+            c.execute('''INSERT INTO `recipients` VALUES(?, ?, ?);''',
+                      (key, name.decode('utf-8'), address.decode('utf-8')))
+            self.query_count += 1
+
+        if self.query_count > 100000000:
+            self.conn.commit()
+            self.query_count = 0
+
+    def _insert_headers(self, c, key, message):
+        """Adds all headers to `headers`.
+        """
+        for header, value in message.items():
+            c.execute('''INSERT INTO `headers` VALUES(?, ?, ?);''', (key, header, value.decode('utf-8')))
+            self.query_count += 1
+
+    def _insert_messages(self, c, key, message):
+        """Creates a basic index of important message data in `messages`.
+        """
+        mail_from = message.get('From', '').decode('utf-8')
+        mail_to = message.get('To', '').decode('utf-8')
+        mail_subject = message.get('Subject', '').decode('utf-8')
+        mail_date_utc = self._get_message_date(message)
+        mail_gmail_id = message.get('X-GM-THRID', '')
+        mail_gmail_labels = message.get('X-Gmail-Labels', '').decode('utf-8')
+
+        c.execute('''INSERT INTO `messages` VALUES(?, ?, ?, ?, ?, ?, ?);''',
+                  (key, mail_from, mail_to, mail_subject, mail_date_utc, mail_gmail_id, mail_gmail_labels))
+        self.query_count += 1
+
     def _get_message_date(self, message):
-        """Finds date and time information for `message` and converts it to ISO-8601 format and UTC timezone."""
+        """Finds date and time information for `message` and converts it to ISO-8601 format and UTC timezone.
+        """
         mail_date = message.get('Date', '').decode('utf-8')
         if not mail_date:
-            # The get_from() result always (so far as I have seen) has the date string in the last 30 characters
+            """The get_from() result always (so far as I have seen!) has the date string in the last 30 characters"""
             mail_date = message.get_from().strip()[-30:]
 
         mail_date_iso8601 = ''
