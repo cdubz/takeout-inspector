ccbadc02e2ccbe2e5147951a62d0836c9be0dc0b
Chris Wells <chris@chrxs.net>
2016-11-15 19:24:02 -0500

    Update example to use the new `all_graphs()` method.

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 
 This is a very basic example of Takeout Inspector's functionality. The code snippet below will
  1) Import mail from a Google Takeout Mail (mbox) file (specified in the settings.cfg file).
- 2) Create and open (in a web browser) a Plotly-based "Top 20 Recipients" graph.
+ 2) Create a file, `all_graphs_mail.html`, containing all current mail (Plotly) graphs.
 
 Note: Large mail files will take a bit of time to process.
 """
@@ -13,4 +13,4 @@ p = mail.Import()
 p.import_messages()
 
 g = mail.Graph()
-g.top_recipients(20)
+g.all_graphs()
