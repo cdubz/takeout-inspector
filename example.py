fb5465f399fde7bd02d2664d2b7d9a5ccb4b7138
Chris Wells <chris@chrxs.net>
2016-11-25 06:49:40 -0500

    Update example comment block for changes from last commit.

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 
 This is a very basic example of Takeout Inspector's functionality. The code snippet below will
  1) Import mail from a Google Takeout Mail (mbox) file (specified in the settings.cfg file).
- 2) Create a file, `all_graphs_mail.html`, containing all current mail (Plotly) graphs.
+ 2) Create a report, `mail.html`, containing all current Mail graphs.
 
 Note: Large mail files will take a bit of time to process.
 """
