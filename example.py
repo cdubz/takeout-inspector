620195c931c06e851837100f2d0cd7030ef5f712
Chris Wells <chris@chrxs.net>
2016-11-25 07:13:08 -0500

    Update example for last commit.

--- example.py
+++ example.py
@@ -2,15 +2,19 @@
 
 This is a very basic example of Takeout Inspector's functionality. The code snippet below will
  1) Import mail from a Google Takeout Mail (mbox) file (specified in the settings.cfg file).
- 2) Create a report, `mail.html`, containing all current Mail graphs.
+ 2) Create Mail report, `mail.html`, containing all current Mail graphs.
+ 3) Create Talk report, `talk.html`, containing all current Talk graphs.
 
 Note: Large mail files will take a bit of time to process.
 """
-from takeout_inspector import mail
+from takeout_inspector import mail, talk
 
 
 p = mail.Import()
 p.import_messages()
 
-g = mail.Graph()
-g.all_graphs()
+m = mail.Graph()
+m.all_graphs()
+
+t = talk.Graph()
+t.all_graphs()
