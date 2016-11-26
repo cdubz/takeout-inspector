a0cfc2682989e083e37a2e6003928b9e0abcc3ad
Chris Wells <chris@chrxs.net>
2016-11-26 17:12:57 -0500

    Update `example.py` for centralized report handling.

--- example.py
+++ example.py
@@ -2,19 +2,12 @@
 
 This is a very basic example of Takeout Inspector's functionality. The code snippet below will
  1) Import mail from a Google Takeout Mail (mbox) file (specified in the settings.cfg file).
- 2) Create Mail report, `mail.html`, containing all current Mail graphs.
- 3) Create Talk report, `talk.html`, containing all current Talk graphs.
+ 2) Create a report with graphs (currently for Mail and Talk data).
 
 Note: Large mail files will take a bit of time to process.
 """
-from takeout_inspector import mail, talk
+from takeout_inspector import mail, report
 
 
-p = mail.Import()
-p.import_messages()
-
-m = mail.Graph()
-m.all_graphs()
-
-t = talk.Graph()
-t.all_graphs()
+mail.Import().import_messages()  # Includes Google Talk data.
+report.Report().generate()
